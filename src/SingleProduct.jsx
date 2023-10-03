5c4cc3c89f38414f70220a5dde3a8e8b87f38c57
mandar-purandare <mnpsstack@gmail.com>
2023-10-03 16:18:18 +0530

    single product page completion

--- src/SingleProduct.jsx
+++ src/SingleProduct.jsx
@@ -10,11 +10,54 @@ const dataObj = {
                 'https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F46%2Fa3%2F46a3eea2d9c78e05cb3370b745182e9f576e2bc0.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_longsleeve%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D',
                 'https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F23%2F1f%2F231f17c1d96931800c3796cfbc7e6738da5f2a31.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_longsleeve%5D%2Ctype%5BDESCRIPTIVEDETAIL%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D',
                 'https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F49%2F65%2F49652a2c77ad819cba31d2d7aeec866e39cbbdfb.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_longsleeve%5D%2Ctype%5BDESCRIPTIVEDETAIL%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D'],
-    "smallImages":[],
+    "smallImages":['https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Ff4%2F66%2Ff4665deecea65f2c98e507f8442d2f530d703b90.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_longsleeve%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D',
+                   'https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F46%2Fa3%2F46a3eea2d9c78e05cb3370b745182e9f576e2bc0.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_longsleeve%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D',
+                  'https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Fb2%2F9d%2Fb29dea35185ac2a9caae0915dc60286fe6a8cfa9.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_longsleeve%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fminiature%5D'],
     "productName": 'Relaxed Fit Jersey Top',
     "productPrice": 1499,
-    "row2Images": [],
-    "row3Images": []
+    "memberPrice": 1279.00,
+    "variants": 'Brown/Cream striped',
+    "sizes": ['XS','S','M','L','XL','XXL'],
+    "row2Data": [{
+                    "url":'https://lp2.hm.com/hmgoepprod?set=source[/21/c3/21c31c065f525fa5bec312a248804601a9a3b3f9.jpg],origin[dam],category[men_jeans_loose],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]',
+                    "name":'Loose Jeans',
+                    "price": 1999.00
+                  },
+                  {
+                    "url":'https://lp2.hm.com/hmgoepprod?set=source[/73/22/7322d5afe5e85ac0e773eb7b6d8ff41556a346ca.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]',
+                    "name":'Twill cap',
+                    "price": 799.00
+                  },
+                  {
+                    "url":'https://lp2.hm.com/hmgoepprod?set=source[/85/53/85532b1d55ead83298dcf1a05c255ced10990b8b.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]',
+                    "name":'Canvas trainers',
+                    "price": 2299.00
+                  },
+                  {
+                    "url":'https://lp2.hm.com/hmgoepprod?set=source[/6b/73/6b73497480377f7a1929dbff62a1b38ea62351df.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]',
+                    "name":'Bangle',
+                    "price": 799.00
+                  }],
+    "row3Data": [{
+                  "url":'https://lp2.hm.com/hmgoepprod?set=source[/ba/75/ba75709f8b00ed2fb7990d1eac67f7fd977bb07d.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[m],hmver[2]&call=url[file:/product/style]',
+                  "name":'Loose Fit T-shirt',
+                  "price": 799.00
+                },
+                {
+                  "url":'https://lp2.hm.com/hmgoepprod?set=source[/f4/66/f4665deecea65f2c98e507f8442d2f530d703b90.jpg],origin[dam],category[men_tshirtstanks_longsleeve],type[DESCRIPTIVESTILLLIFE],res[m],hmver[2]&call=url[file:/product/style]',
+                  "name":'Relaxed Fit Jersey top',
+                  "price": 1499.00
+                },
+                {
+                  "url":'https://lp2.hm.com/hmgoepprod?set=source[/b2/9d/b29dea35185ac2a9caae0915dc60286fe6a8cfa9.jpg],origin[dam],category[men_tshirtstanks_longsleeve],type[DESCRIPTIVESTILLLIFE],res[m],hmver[2]&call=url[file:/product/style]',
+                  "name":'Relaxed Fit Jersey top',
+                  "price": 1499.00
+                },
+                {
+                  "url":'https://lp2.hm.com/hmgoepprod?set=source[/30/3b/303b85d60eb5e0836e0f2756ff207fc0b59c0c41.jpg],origin[dam],category[men_hoodiessweatshirts_sweatshirts],type[DESCRIPTIVESTILLLIFE],res[m],hmver[2]&call=url[file:/product/style]',
+                  "name":'Relaxed Fit Sweatshirt',
+                  "price": 799.00
+                }]
 }
 
 function SingleProduct() {
--- src/SingleProductBody.jsx
+++ src/SingleProductBody.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './SingleProductBody.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import star_rating from './Images/single_product/star_rating.png'
+import true_to_size from './Images/single_product/true_to_size_scale.png';
 
 function SingleProductBody({dataObj}) {
     
@@ -19,27 +19,74 @@ function SingleProductBody({dataObj}) {
             <div className='row-1-right-div black-border'>
                 <div className='title-and-heart'>
                     <h4>{dataObj.productName}</h4>
-                    <i class="fa-regular fa-heart"></i>
+                    <i class="fa-regular fa-heart fa-xl"></i>
                     {/* <i class="fa-solid fa-heart"></i> */}
                 </div>
-                <div className='price-and-member-price'></div>
-                <h5 className='variants'></h5>
-                <div className='variants-img-container'></div>
-                <div className='sizes'></div>
-                <p className='size-guide'></p>
-                <button className='add-to-cart-btn'>Add</button>
-                <p className='find-a-store'></p>
-                <p className='standard-delivery-info'></p>
+                <div className='price-and-member-price'>
+                    <h3>Rs.{dataObj.productPrice}</h3>
+                    <h4>Member Price Rs.{dataObj.memberPrice}</h4>
+                </div>
+                <h5 className='variants'>{dataObj.variants}</h5>
+                <div className='variants-img-container'>
+                    { dataObj.smallImages.map((url) => (
+                        <div className='black-border'>
+                            <img src={url}/>
+                        </div>
+                    ))}
+                    
+                </div>
+                <div className='sizes'>
+                    <h5>Sizes</h5>
+                    <div className='sizes-div-container'>
+                        {
+                            dataObj.sizes.map((size) => (
+                                <div>{size}</div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <p className='size-guide'><i class="fa-solid fa-ruler"></i>Size guide</p>
+                <button className='add-to-cart-btn'><i class="fa-solid fa-bag-shopping"></i>Add</button>
+                <p className='find-a-store'><i class="fa-solid fa-store"></i>Find in store</p>
+                <p className='standard-delivery-info'><i class="fa-solid fa-circle-info"></i>Standard delivery in 2-7 days</p>
                 <h4>Delivery and Payment</h4>
-                <p className='reviews'>(215 reviews)</p>
-                <div className='true-to-size-scale'></div>
-                <div className='product-related-info'></div>
-                <div className='product-related-info'></div>
-                <div className='product-related-info'></div>
+                <div className='reviews'><img src={star_rating}/><p>(215 reviews)</p></div>
+                <div className='true-to-size-scale'>
+                    <img src={true_to_size}/>
+                </div>
+                <div className='product-related-info'><h3>Description & fit</h3><i class="fa-solid fa-chevron-down"></i></div>
+                <div className='product-related-info'><h3>Materials & suppliers</h3><i class="fa-solid fa-chevron-down"></i></div>
+                <div className='product-related-info'><h3>Care guide</h3><i class="fa-solid fa-chevron-down"></i></div>
             </div>
         </div>
-        <div className='row-2'></div>
-        <div className='row-3'></div>
+        <div className='row-2'>
+            <h3>Style with</h3>
+            <div className='sliding-images-container'>
+                {
+                    dataObj.row2Data.map((obj) =>(
+                        <div>
+                            <div className='black-border'><img src={obj.url}/></div>
+                            <h5>{obj.name}</h5>
+                            <h5>Rs.{obj.price}</h5>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+        <div className='row-3'>
+        <h3>Others also bought</h3>
+            <div className='sliding-images-container'>
+                {
+                    dataObj.row3Data.map((obj) =>(
+                        <div>
+                            <div className='black-border'><img src={obj.url}/></div>
+                            <h5>{obj.name}</h5>
+                            <h5>Rs.{obj.price}</h5>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     </div>
   )
 }
